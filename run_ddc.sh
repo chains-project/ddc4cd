acdbeac45e289b4267f96aaf44ee83dc8294eae1
ludvigch <ludvigch@kth.se>
2025-02-28 15:14:27 +0100

    add script to run ddc with nix locally

---
+++ run_ddc.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# run ddc locally, has side effects in /tmp
+# dependency: nix
+
+echo "Running DDC using nix locally"
+./.github/workflows/scripts/clean.sh
+nix-shell --pure --run "./.github/workflows/scripts/setup.sh"
+nix-shell --pure --run "./.github/workflows/scripts/ddc.sh -e local_machine -i ./.github/workflows/scripts/init.sh"
+./.github/workflows/scripts/clean.sh
