a3bc3e92227295246dcdb719070abcbe1cf1253a
ludvigch <ludvigch@kth.se>
2025-03-11 10:33:26 +0100

    update script to conform to new ddc.sh

--- run_ddc.sh
+++ run_ddc.sh
@@ -5,4 +5,4 @@
 echo "Running DDC using nix locally"
 ./.github/workflows/scripts/clean.sh
 nix-shell --pure --run "./.github/workflows/scripts/setup.sh"
-nix-shell --pure --run "./.github/workflows/scripts/ddc.sh -e local_machine -i ./.github/workflows/scripts/init.sh"
+nix-shell --pure --run "./.github/workflows/scripts/ddc.sh -e local_machine -h f6385c05308f715bdd2c06336801193a21d69b50"
