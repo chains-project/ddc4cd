64da94207b64707dd4688b352e824a2d20175be4
ludvigch <ludvigch@kth.se>
2025-03-11 13:23:57 +0100

    add nixpkgs hash as workflow input

--- run_ddc.sh
+++ run_ddc.sh
@@ -4,5 +4,5 @@
 
 echo "Running DDC using nix locally"
 ./.github/workflows/scripts/clean.sh
-nix-shell --pure --run "./.github/workflows/scripts/setup.sh"
-nix-shell --pure --run "./.github/workflows/scripts/ddc.sh -e local_machine -h f6385c05308f715bdd2c06336801193a21d69b50"
+nix-shell --argstr pkgs-rev "07518c851b0f12351d7709274bbbd4ecc1f089c7" --pure --run "./.github/workflows/scripts/setup.sh"
+nix-shell --argstr pkgs-rev "07518c851b0f12351d7709274bbbd4ecc1f089c7" --pure --run "./.github/workflows/scripts/ddc.sh -e local_machine -h f6385c05308f715bdd2c06336801193a21d69b50"
