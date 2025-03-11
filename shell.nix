1081f66bd06d50563193fbe5258aaf3dd5c1e9ba
ludvigch <ludvigch@kth.se>
2025-03-11 09:57:26 +0100

    add wget nix package

--- shell.nix
+++ shell.nix
@@ -7,6 +7,7 @@ pkgs.mkShell {
   buildInputs = [ 
     pkgs.stdenv
     pkgs.clang
+    pkgs.wget
   ];
   shellHook = ''
     export STAGE1_CONF="--crtprefix=${lib.getLib pkgs.stdenv.cc.libc}/lib --sysincludepaths=${lib.getDev pkgs.stdenv.cc.libc}/include:{B}/include --libpaths={B}:${lib.getLib pkgs.stdenv.cc.libc}/lib --elfinterp=${lib.getLib pkgs.stdenv.cc.libc}/lib64/ld-linux-x86-64.so.2"
