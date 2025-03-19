8e1c9f1aa796b46000c2dd4135102f4c26055b66
ludvigch <ludvigch@kth.se>
2025-03-19 10:40:27 +0100

    add realpath to config to resolve differing mac paths

--- .github/workflows/scripts/config.sh
+++ .github/workflows/scripts/config.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 # Global configuration for ddc scripts
-build_dir=/tmp/build
+build_dir=$(realpath /tmp/build)
 prefix=${build_dir}/tcc-root
 ln_location=${build_dir}/tcc-root
 extra_flags="-s"
 trusted_compilers="gcc clang"
-src_a_dir="/tmp/src_a"
+src_a_dir=$(realpath /tmp/src_a)
