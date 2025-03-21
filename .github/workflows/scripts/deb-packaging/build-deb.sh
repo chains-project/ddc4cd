568f5e07a1b81fcb02ba36b5169ed80eb448aa67
ludvigch <ludvigch@kth.se>
2025-03-21 13:04:05 +0100

    pass version in step output

--- .github/workflows/scripts/deb-packaging/build-deb.sh
+++ .github/workflows/scripts/deb-packaging/build-deb.sh
@@ -52,7 +52,7 @@ done
 chmod +x $tcc_dir/usr/bin/tcc
 version=$($tcc_dir/usr/bin/tcc -v | awk '{print $3}')+git$(echo $version_commit | cut -c1-8)
 deb_name=tcc-hardened_${version}
-echo "CURRENT_RELEASE=$version" >> $GITHUB_ENV
+echo "CURRENT_RELEASE=$version" >> $GITHUB_OUTPUT
 # create necessary directories for the archive
 cd /tmp
 rm -rf ./$deb_name
