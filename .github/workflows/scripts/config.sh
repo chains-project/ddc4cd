b33f40b184e8d48da1085ce829c4fc18a2b800bd
ludvigch <ludvigch@kth.se>
2025-04-01 10:22:21 +0200

    remove clang for now when testing

--- .github/workflows/scripts/config.sh
+++ .github/workflows/scripts/config.sh
@@ -4,5 +4,5 @@ build_dir=$(realpath /tmp/build)
 prefix=${build_dir}/tcc-root
 ln_location=${build_dir}/tcc-root
 extra_flags="-s"
-trusted_compilers="gcc clang"
+trusted_compilers="gcc"
 src_a_dir=$(realpath /tmp/src_a)
