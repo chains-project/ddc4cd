ccaf3279317a275ec77e589c9f3e5376d7d6c8a7
ludvigch <ludvigch@kth.se>
2025-04-01 14:11:35 +0200

    dont fail if debug fails

--- .github/workflows/scripts/double-compile.sh
+++ .github/workflows/scripts/double-compile.sh
@@ -23,16 +23,16 @@ EOF
 cd $source_dir
 make clean
 ./configure --targetos=Linux --cc=${initial_compiler} --prefix=${prefix} --extra-ldflags=${extra_flags} $STAGE1_CONF
-./configure --help
-ls -la
+./configure -h || true
+cat config.mak || true
 make cross-x86_64
 #objcopy -D libtcc.a
 make install DESTDIR=${stage1_dir}
 ln -sfT ${stage1_dir}${prefix} ${ln_location}
 make clean
 ./configure --cc="${stage1_dir}${prefix}/bin/x86_64-tcc" --extra-ldflags=${extra_flags}
-./configure --help
-ls -la
+./configure -h || true
+cat config.mak || true
 make
 #objcopy -D libtcc.a
 make install DESTDIR=${stage2_dir}
