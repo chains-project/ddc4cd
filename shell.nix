bc622ca2b11733b07ba39773853499644a3057f2
ludvigch <ludvigch@kth.se>
2025-05-02 09:04:24 +0200

    fix: add configure params for fedora reproducibility

--- shell.nix
+++ shell.nix
@@ -15,5 +15,7 @@ pkgs.mkShell {
   shellHook = ''
     export STAGE1_CONF="--crtprefix=${lib.getLib pkgs.stdenv.cc.libc}/lib --sysincludepaths=${lib.getDev pkgs.stdenv.cc.libc}/include:{B}/include --libpaths={B}:${lib.getLib pkgs.stdenv.cc.libc}/lib"
     echo $STAGE1_CONF
+    export STAGE2_CONF="--crtprefix=/usr/lib/x86_64-linux-gnu --sysincludepaths={B}/include:/usr/local/include/x86_64-linux-gnu:/usr/local/include:/usr/include/x86_64-linux-gnu:/usr/include --libpaths={B}:/usr/lib/x86_64-linux-gnu:/usr/lib:/lib/x86_64-linux-gnu:/lib:/usr/local/lib/x86_64-linux-gnu:/usr/local/lib"
+    echo $STAGE2_CONF
   '';
 }
