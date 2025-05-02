3bf76d59f91f38e9b108956bdf04deff58ffaefc
ludvigch <ludvigch@kth.se>
2025-05-02 10:23:48 +0200

    fix: use only upstream provided binary compilers as GP

--- .github/workflows/scripts/config.sh
+++ .github/workflows/scripts/config.sh
@@ -4,5 +4,5 @@ build_dir=/tmp/build
 prefix=${build_dir}/tcc-root
 ln_location=${build_dir}/tcc-root
 extra_flags="-s"
-trusted_compilers="gcc clang"
+trusted_compilers="gcc clang tcc"
 src_a_dir="/tmp/src_a"
